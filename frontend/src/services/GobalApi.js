019342cef8519a79ad94e091e5df04fc306f0f62
Rakesh <patelrakesh6300@gmail.com>
2026-06-29 00:09:33 +0530

    AA

--- frontend/src/services/GobalApi.js
+++ frontend/src/services/GobalApi.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "http://localhost:8000/api/v1",
+    baseURL: "https://smart-hostel-quality-management-system.onrender.com/api/v1",
     headers: {
         "Content-Type": "application/json",
     },
 });
 
 export default api;
+
